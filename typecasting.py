d21713161bd78d01e1db306bd63bcb894dd96821
kate-kiri <kirikate68@gmail.com>
2026-09-17 08:56:29 +0300

    typecating data types

---
+++ typecasting.py
@@ -0,0 +1,25 @@
+#typecasting is the process of converting a data type to another
+
+candidate = True
+age = 20
+meanscore = 67.3
+name =" Kiri"
+
+print(type(age))
+print(type(candidate))
+print (type(meanscore))
+print (type(name))
+
+
+meanscore =int(meanscore) 
+print(meanscore)
+
+candidate = str(candidate)
+print(candidate)
+
+age = float(age)
+print(age)
+
+name = bool(name)
+print(name)
+
